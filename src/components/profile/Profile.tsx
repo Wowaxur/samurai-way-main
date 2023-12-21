507f786d525699656d150593f50c3fa169b01062
Wowaxur <wowaxur@yandex.ru>
2023-12-21 09:10:03 +0300

    fix

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,24 +1,23 @@
 import React from "react";
-import FrontImg from '../images/front-end-development.jpg'
 import LokiFace from '../images/lokiface.png'
-import s from './MainContent.module.css'
+import s from './Profile.module.css'
 import {ProfileTextBlock} from "./porfileTextBlock/ProfileTextBlock";
 import PostsBlock from "./myPosts/postsBlock/PostsBlock";
 
 
+const Profile = () => {
 
-const MainContent = () => {
-
-    return(
+    return (
         <div className={s.MainContent}>
-            <img className={s.FrontImg} src={"https://i.pinimg.com/736x/47/df/c8/47dfc8171983fd4c6b0056128893b02b.jpg"} alt={'1232'}/>
+            <img className={s.FrontImg} src={"https://i.pinimg.com/736x/47/df/c8/47dfc8171983fd4c6b0056128893b02b.jpg"}
+                 alt={'1232'}/>
             <div className={s.ProfileWrapper}>
                 <img className={s.LokiFace} src={LokiFace} alt={'123'}/>
                 <ProfileTextBlock/>
             </div>
-           <PostsBlock/>
+            <PostsBlock/>
 
         </div>
     )
 }
-export default MainContent
+export default Profile
