9ee2dfb777224b2005883a7b2e8962b809152a60
Wowaxur <wowaxur@yandex.ru>
2024-01-07 13:35:52 +0300

    add map to dialogs from state

--- src/components/Pages/dialogs/Dialogs.tsx
+++ src/components/Pages/dialogs/Dialogs.tsx
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Dialogs.module.css';
+import state, {userDb} from "../../../redux/state";
+import {message} from "antd";
+type dialogsType={
+    id: number
+    name: string
+}
+type MessageType = {
+    id: number
+    message: string
+}
 
+type dialogsPageType = {
+    dialogs: Array<dialogsType>
+    messages: Array<MessageType>
+}
 
 const Dialogs = () => {
-    return (
-        <div className={s.MainContent}>
-            Dialogs
-            Dialogs
-            Dialogs
-            <br/>
-            Dialogs
-            Dialogs
-            Dialogs
-            Dialogs
-            Dialogs
+    let [dbdialogs, setdbDialogs] = useState<dialogsPageType>(state.dialogsPage)
+    let [selectedUserId, setSelectedUserId] = useState<null | number>(null);
 
+    return (
+        <div className={s.DialogsContent}>
+            <div className={s.UserDialogsList}>
+                <h2> Dialogs</h2>
+                {dbdialogs.dialogs.map(dialog => (
+                    <div
+                        key={dialog.id}
+                        className={`${s.UserDialog} ${dialog.id === selectedUserId ? s.ActiveUserDialog : ''}`}
+                        onClick={() => setSelectedUserId(dialog.id)}
+                    >
+                        {dialog.name}
+                    </div>
+                ))}
+            </div>
+            <div className={s.MesageList}>
+                <h2> Messages</h2>
+                {selectedUserId && dbdialogs.messages.filter(message => message.id === selectedUserId).map(message => {
+                    const user = dbdialogs.dialogs.find(user => user.id === message.id);
+                    return (
+                        <div key={message.id} className={s.UserDialog}>
+                            {user ? `${user.name}: ${message.message}` : message.message}
+                        </div>
+                    )
+                })}
+                <input/>
+            </div>
         </div>
     );
 };
