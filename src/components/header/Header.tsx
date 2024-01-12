4c6ff794ba12eb15c01428805ed961d7944badcd
Wowaxur <wowaxur@yandex.ru>
2024-01-12 17:26:24 +0300

    Rework

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,9 +4,7 @@ import logoImg from '../images/logo.png'
 const Header =  () => {
     return (<header className={s.Header}>
         <img className= {s.Logo} src={logoImg}  alt={'123'}/>
-        <a className={s.link} href={'#s'}>Home</a>
-        <a className={s.link} href={'#s'}>News</a>
-        <a className={s.link} href={'#s'}>Feed</a>
+
         {/*<a className={s.link} href={'#s'}>Messages</a>*/}
     </header>)
 
